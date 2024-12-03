c38471d9f882da49cf1ba49dd0a4919c880eacc2
Max <max@huzm.ru>
2024-12-03 21:04:21 +0300

    Fill and show array

--- WorkWithArray.cpp
+++ WorkWithArray.cpp
@@ -1,9 +1,22 @@
 ﻿
-
 #include <iostream>
+using std::cout;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int SIZE = 8;
+    int mas[SIZE];
+    // Fill array
+    for (int i = 0; i < SIZE; i++) {
+        mas[i] = SIZE - i;
+    }
+    // Show array elements
+    for (int i = 0; i < SIZE; i++) {
+        cout << mas[i] << ", ";
+    }
+    cout << (char)8 << (char)8;
+
+
+
 }
 
